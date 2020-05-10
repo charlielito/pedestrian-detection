30230216ed9e56ba33e4b067e78eb9bcecdfcead
charlielito <candres.alv@gmail.com>
2020-05-10 12:04:01 -0500

    fix export

--- scripts/export_model.sh
+++ scripts/export_model.sh
@@ -2,16 +2,12 @@
 
 INPUT_TYPE=image_tensor
 # INPUT_TYPE=encoded_image_string_tensor
-OUTPUT_DIR=/code/data/export_models/ssd_mobilenet_v2_pedestrian
-
+EXPORT_DIR=/code/data/export_models/ssd_mobilenet_v2_pedestrian
 PIPELINE_CONFIG_PATH=/code/data/ssd_mobilenet_v2.config
-CHECKPOINT=/code/data/train_data/model.ckpt-16283
+TRAINED_CKPT_PREFIX=/code/data/train_data/model.ckpt-16283
 
-# PIPELINE_CONFIG_PATH=data/ssd_mobilenet_v2.config
-# CHECKPOINT=train_data/data/train_data/model.ckpt-16283
-
-python /code/scripts/export_model.py \
-    --input_type $INPUT_TYPE \
-    --pipeline_config_path $PIPELINE_CONFIG_PATH  \
-    --trained_checkpoint_prefix $CHECKPOINT \
-    --output_directory $OUTPUT_DIR
+python object_detection/export_inference_graph.py \
+    --input_type=${INPUT_TYPE} \
+    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+    --trained_checkpoint_prefix=${TRAINED_CKPT_PREFIX} \
+    --output_directory=${EXPORT_DIR}
